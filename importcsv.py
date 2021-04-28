649625bc0ef8439356f3d86ef294502cf9bc7dc7
Joanne Stenzel <joanne.stenzel@e-systems.de>
2021-04-28 14:43:40 +0200

    Erste Überlegungen für einzelne Auslesung

--- importcsv.py
+++ importcsv.py
@@ -2,3 +2,8 @@ import csv
 reader = csv.reader(open("dht22.csv"), delimiter=";")
 for row in reader:
 	print(row)
+#alternativ einzelne Spalten auslesen
+#for (k,v) in row.items():
+	#columns[k].append(v)
+#print(columns["timestamp"]
+#print(columns["temperature"]
